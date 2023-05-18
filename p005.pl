0984be82ff9eaccccb5e9f28ee83ad7e44e87a79
Jeremy Reeder <jeremy.reeder@pm.me>
2023-05-18 11:44:43 -0600

    Rewrote for efficiency.

--- p005.pl
+++ p005.pl
@@ -1,20 +1,15 @@
 #!/usr/bin/env perl
 use Modern::Perl '2020';
+use List::Util qw(product);
+use Math::Prime::Util qw(primes);
 
 # Problem 5: What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-sub isDivisibleByOneThruTwenty {
-	my ($n) = @_;
-	for my $d (1..20) {
-		if ($n % $d != 0) { return 0 }
-	}
-	1;
-}
-
 sub answer {
-	my $x = 1;
-	while (!isDivisibleByOneThruTwenty($x)) { $x++ }
-	$x;
+	my ($max_factor) = @_;
+	my @primes = @{primes($max_factor)};
+	my @factors = map { $_ ** int(log($max_factor) / log($_)) } @primes;
+	product(@factors);
 }
 
-say answer;
+say answer(20);
