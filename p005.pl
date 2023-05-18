8a8cecde10d792d8a81a29a4a56cc5b1ae182c9b
Jeremy Reeder <jeremy.reeder@pm.me>
2023-05-18 10:39:06 -0600

    Added a broken solution to problem 5.

--- p005.pl
+++ p005.pl
@@ -1,3 +1,20 @@
-#!/usr/bin/perl
-use strict;
-use warnings FATAL => 'all';
+#!/usr/bin/env perl
+use Modern::Perl '2020';
+
+# Problem 5: What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+sub isDivisibleByOneThruTwenty {
+	my ($n) = @_;
+	for my $d (1..20) {
+		if ($n % $d != 0) { return false }
+	}
+	true;
+}
+
+sub answer {
+	my $x = 1;
+	while (!isDivisibleByOneThruTwenty($x)) { $x++ }
+	$x;
+}
+
+say answer;
