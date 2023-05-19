66c62ee37b441fd1be8973967b1a88e2497867b3
Jeremy Reeder <jeremy.reeder@pm.me>
2023-05-19 04:36:22 -0600

    Now passing all parameters at once.

--- p001.pl
+++ p001.pl
@@ -6,8 +6,7 @@ use List::MoreUtils qw(uniq);
 # Problem 1: Find the sum of all the multiples of 3 or 5 below 1000.
 
 sub multiples {
-	my $n = $_[0];
-	my $ceiling = $_[1];
+	my ($n, $ceiling) = @_;
 	map { $n * $_ } 1..(($ceiling - 1) / $n);
 }
 
